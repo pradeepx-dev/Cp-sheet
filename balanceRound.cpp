509324c64a3ba36cdd6b2b1e789d68f2e3618a1b
Pradeep Kumar Maurya <pradeepkmaurya2003@gmail.com>
2024-09-09 16:25:34 +0530

    Today Commit

---
+++ balanceRound.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+
+        int arr[n];
+        for(int i=0;i<n;i++) cin>>arr[i];
+        sort(arr,arr+n);
+        // for(int i=0;i<n;i++){
+        //     cout<<arr[i]<<" ";
+        // }
+        // cout<<"\n";
+        int round=0;
+        int ans=1;
+        for(int i=1;i<n;i++){
+            int oye = arr[i] - arr[i-1];
+            if(oye <= k) ans++;
+            else{
+                round = max(round,ans);
+                ans=1;
+            }
+        }
+        round = max(round,ans);
+        cout<<n-round<<endl;
+    }
+    return 0;
+}
