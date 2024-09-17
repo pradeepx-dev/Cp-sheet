c5375ae9d01357990c7d621cebe16f7bb8c53662
Pradeep Kumar Maurya <pradeepkmaurya2003@gmail.com>
2024-09-17 22:19:18 +0530

    jellyfish and undertale1

--- jellyfishUndertale.cpp
+++ jellyfishUndertale.cpp
@@ -1,22 +1,22 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL);
-//     int t;
-//     cin>>t;
-//     while(t--){
-//         long long a,b,n;
-//         cin>>a>>b>>n;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        long long a,b,n;
+        cin>>a>>b>>n;
 
-//         long long sum=b;
-//         long long arr[n];
-//         for(int i=0;i<n;i++){
-//             cin>>arr[i];
-//             sum += min(a-1, arr[i]);
-//         }
-//         cout<<sum<<endl;
-//     }
-//     return 0;
-// }
+        long long sum=b;
+        long long arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+            sum += min(a-1, arr[i]);
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}
