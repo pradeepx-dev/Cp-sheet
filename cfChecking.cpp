881afa44732dadb5c11a92d92c130e844e91ff59
Pradeep Kumar Maurya <pradeepkmaurya2003@gmail.com>
2024-10-12 23:32:42 +0530

    cf checking

---
+++ cfChecking.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){ 
+    int t;
+    cin>>t;
+   while(t--){
+     char h;
+     cin>>h;
+    if(h=='c'||h=='o'||h=='d'||h=='e'||h=='f'||h=='r'||h=='s') cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+  }
+  return 0;
+}
