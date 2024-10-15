0bb886ebad32849a21c4dba0db82b6886436bddf
Pradeep Kumar Maurya <pradeepkmaurya2003@gmail.com>
2024-10-15 23:58:02 +0530

    midium

---
+++ medium.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+int n;
+cin>>n;
+
+int a[3];
+
+while(n--){
+
+    for(int i=0;i<3;i++){
+        cin>>a[i];
+        }
+    sort(a,a+3);
+
+    cout<<a[1]<<endl;
+}
+
+return 0;
+}
